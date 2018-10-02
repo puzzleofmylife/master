a90dd194da6d2aa503df091f7a066cdb9e3d2b29
Dane Williams <danewilliams@danes-mbp.gray.za.ibm.com>
2018-10-02 14:35:33 +0200

    Add confirmEmail function to service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  confirmEmail(userId: string, confirmToken: string): Observable<any> {
+    return this.http.get(environment.baseAPIURL + '/api/User/confirmemail', { params: {userId: userId, confirmToken: confirmToken} });
+  }
 }
