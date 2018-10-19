6b7b7dee4c43611b88d287c8db4b92158c497ea6
Dane Williams <danewilliams@danes-mbp.gray.za.ibm.com>
2018-10-19 14:47:31 +0200

    Choose psych init commit

--- src/app/services/psycho.service.ts
+++ src/app/services/psycho.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { RegisterPsycho } from '../models/RegisterPsycho';
 import Psychologist from '../models/Psychologist';
+import PsychologistPublic from '../models/PsychologistPublic';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +11,11 @@ export class PsychoService {
 
   constructor(private http: HttpClient) { }
 
-  getAll() {
-    //return this.http.get<Psycho[]>(`/psychos`);
-  }
-
-  getById(id: number) {
-    return this.http.get(`/psychos` + id);
+  getAvailable(limit: number) {
+    return this.http.get<PsychologistPublic[]>(environment.baseAPIURL + '/api/Psychologist/available', { params: new HttpParams().set('limit', limit.toString()) });
   }
 
   register(psycho: Psychologist) {
-    var x = environment.baseAPIURL + '/api/User/psychologist';
-    var y = psycho;
     return this.http.post(environment.baseAPIURL + '/api/User/psychologist', psycho);
   }
 }
