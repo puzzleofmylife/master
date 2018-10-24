db9ecb5fb11c3c5b530afd4d94bd8f18bf170218
Dane Williams <danewilliams@danes-mbp.gray.za.ibm.com>
2018-10-24 15:11:32 +0200

    Remove angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
 import { PsyregisterComponent } from './components/psyregister/psyregister.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlertService } from './services/alert.service';
@@ -45,8 +43,6 @@ import { CheckoutResultComponent } from './components/checkout-result/checkout-r
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatMenuModule,
-    MatButtonModule,
     ReactiveFormsModule,
     HttpClientModule
   ],
