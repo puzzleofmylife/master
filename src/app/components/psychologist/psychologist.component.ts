ff91efd741d45d0740f40fb4b59b60913e69fb39
Dane Williams <danewilliams@danes-mbp.gray.za.ibm.com>
2018-11-09 16:05:05 +0200

    Add styling to psychologist profile

--- src/app/components/psychologist/psychologist.component.ts
+++ src/app/components/psychologist/psychologist.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LoggedIn } from 'src/app/models/LoggedIn';
 import { Psychologist } from 'src/app/models/Psychologist';
-
 import { AuthService } from './../../services/auth.service';
 import { PsychoService } from './../../services/psycho.service';
+import { HelpersService } from 'src/app/services/helpers.service';
 
 @Component({
 	selector: 'app-psychologist',
@@ -15,8 +15,9 @@ export class PsychologistComponent implements OnInit {
 
 	psychologist: Psychologist = new Psychologist();
 	loggedIn: LoggedIn = new LoggedIn();
+	loaded: Boolean = false;
 
-	constructor(private authService: AuthService, private psychoService: PsychoService, private route: ActivatedRoute) { }
+	constructor(private authService: AuthService, private psychoService: PsychoService, private route: ActivatedRoute, private helpersService: HelpersService) { }
 
 	ngOnInit() {
 
@@ -25,9 +26,16 @@ export class PsychologistComponent implements OnInit {
 		const id = +(this.route.snapshot.params["id"]);
 		this.psychoService.getById(id).subscribe((psychologist) => {
 			this.psychologist = psychologist;
-
+			this.psychologist.attachments = this.psychologist.attachments.filter(x => x.typeId != 2);
+			this.loaded = true;
 		})
 
 	}
 
+	getAttachmentDisplayName(attachment: any): string{
+		var attachmentDisplayName = attachment.type + '.' + attachment.fileName.split('.')[1];
+		var maxLength =  10;
+		attachmentDisplayName = attachmentDisplayName.length > maxLength ? attachmentDisplayName.substring(0, maxLength) + '...' : attachmentDisplayName;
+		return attachmentDisplayName;
+	}
 }
---
+++ src/app/services/helpers.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelpersService {
+
+  constructor() { }
+
+  getPsychStatusClass(statusId: number) {
+    switch (statusId) {
+      case 1:
+        return 'pending_approval';
+        break;
+      case 2:
+        return 'active';
+        break;
+    }
+  }
+}
