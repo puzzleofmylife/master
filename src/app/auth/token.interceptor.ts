77b71324fd1cdf5440fcb363cdadfb5b3f64dea7
Dane Williams <danewilliams@danes-mbp.gray.za.ibm.com>
2018-11-21 17:03:13 +0200

    Add intercepter for 401 to redirect to login

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -3,24 +3,39 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
+
     request = request.clone({
       setHeaders: {
         Authorization: `Bearer ${this.auth.getAccessToken()}`
       }
     });
 
-    return next.handle(request);
+    return next.handle(request).do((event: HttpEvent<any>) => {
+      if (event instanceof HttpResponse) {
+        // do stuff with response if we want
+      }
+    }, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          // redirect to the login route
+          this.router.navigate(['/login']);
+        }
+      }
+    });
   }
 }
