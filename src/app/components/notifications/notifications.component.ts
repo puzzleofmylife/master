09910f4137703258449bc1c288db005d0b58170b
Tshepo-Bahula <Tshepo.Bahula@ibm.com>
2018-11-22 09:35:57 +0200

    code cleanup,added logic to check for new notifications per minute.

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HelpersService } from '../../services/helpers.service';
 import { NotificationService } from 'src/app/services/notification.service';
+import { TimerObservable } from "rxjs/observable/TimerObservable";
+import { Subscription } from 'rxjs';
+import * as moment from 'moment';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,19 +14,25 @@ import { NotificationService } from 'src/app/services/notification.service';
 })
 
 
-export class NotificationsComponent implements OnInit {
+export class NotificationsComponent implements OnInit, OnDestroy {
 
 	currentNotifications: any[];
 	newNotificationCount: number;
 	notificationLimit: number = 10;
-	createTime: Date;
 	showNotificationDropdown: boolean = false;
-	notification: boolean = false;
+	newNotificationsSubscription: Subscription;
+	newNotificationGetInterval: number = 60 * 1000;
 
-	constructor(private helpersService: HelpersService, private notificationService: NotificationService) { }
+	constructor(private helpersService: HelpersService, private notificationService: NotificationService, private router: Router) { }
 
 	ngOnInit() {
-
+		this.getNewNotifications();
+		this.newNotificationsSubscription = TimerObservable.create(this.newNotificationGetInterval, this.newNotificationGetInterval)
+			.subscribe(() => {
+			this.getNewNotifications();
+		});
+	}
+	getNewNotifications(): any {
 		this.notificationService.getNewNotificationCount().subscribe(response => {
 			this.newNotificationCount = response.count;
 		}, error => {
@@ -30,20 +40,21 @@ export class NotificationsComponent implements OnInit {
 		});
 
 		this.notificationService.getNotifications(this.notificationLimit, 0).subscribe(response => {
-			this.currentNotifications = response
+			this.currentNotifications = response;
 		}, error => {
 			console.error(JSON.stringify(error));
 		});
-
-		this.createTime = new Date();
 	}
 
-	onCloseNotificationsDropDown(): boolean {
-		this.notificationService.markNotificationAsRead().subscribe(x => {
-			this.markNotificationsRead();
-			this.resetNewNotificationCount();
-		});
-		return this.showNotificationDropdown = false;
+	onOpenCloseNotificationDropDown(): void {
+		this.showNotificationDropdown = !this.showNotificationDropdown;
+
+		if (!this.showNotificationDropdown) {
+			this.notificationService.markNotificationAsRead().subscribe(x => {
+				this.markNotificationsRead();
+				this.resetNewNotificationCount();
+			});
+		}
 	}
 
 	markNotificationsRead(): void {
@@ -52,7 +63,24 @@ export class NotificationsComponent implements OnInit {
 		});
 	}
 
-	resetNewNotificationCount(): number {
-		return this.newNotificationCount = 0;
+	resetNewNotificationCount(): void {
+		this.newNotificationCount = 0;
+	}
+
+	convertToLocalDate(utcDate: Date): Date {
+		return moment.utc(utcDate).local().toDate();
+	}
+
+	navToNotifications() {
+		this.onOpenCloseNotificationDropDown();
+		this.router.navigate(['/notifications']);
+	}
+
+	ngOnDestroy() {
+		/* We need to unsubscribe, otherwise the getting of new notifications will continue
+		*  even if we navigate away from this component
+		*/
+		if (this.newNotificationsSubscription)
+			this.newNotificationsSubscription.unsubscribe();
 	}
 }
