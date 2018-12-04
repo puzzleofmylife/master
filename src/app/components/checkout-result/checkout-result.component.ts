1e698590dd702c4e27f8cfbfbfd005db30c5d664
Dane Williams <danewilliams@danes-mbp.gray.za.ibm.com>
2018-12-04 12:51:00 +0200

    Use result cmp for success/fail

--- src/app/components/checkout-result/checkout-result.component.ts
+++ src/app/components/checkout-result/checkout-result.component.ts
@@ -10,7 +10,10 @@ import { ActivatedRoute } from '@angular/router';
 export class CheckoutResultComponent implements OnInit {
   loaded: boolean = false;
   success: boolean = false;
-  isExpiredCheckout: boolean = false;
+  resultText: string;
+  resultHeading: string;
+  resultButtonText: string = 'Back to profile';
+  resultButtonLink: string = '/profile';
   result: any;
   isSignup: boolean;
   psychName: string;
@@ -23,20 +26,39 @@ export class CheckoutResultComponent implements OnInit {
       var checkoutId = params['id'];
       this.isSignup = params['signup'] == '1' ? true : false;
       this.psychName = params['psych'] ? decodeURI(params['psych']) : null;
-      
-      try { 
+
+      try {
         this.result = await this.paymentService.getCheckoutStatus(checkoutId);
 
         this.loaded = true;
         this.success = this.result.success;
-        this.isExpiredCheckout = this.result.code == '200.300.404' ? true : false;
-        
-        if(!this.result.Success)
+
+        if (this.success && this.isSignup) {
+          this.resultText = `You are now signed up and ready to go!
+          <br /><br />
+          Your psychologist ${this.psychName} can't wait to meet you. Click the button below to start your first
+          session.`;
+          this.resultHeading = 'Thank you';
+          this.resultButtonLink = '/session';
+          this.resultButtonText = 'Start session';
+        } else if (this.success) {
+          this.resultText = 'Your payment details have been updated.';
+        } else {
+          this.resultText = `Your payment details could not be updated. <br/><br/>Reason: '${this.result.peachResponse.result.description}'`;
+        }
+
+        if (this.result.peachResponse.result.code == '200.300.404') {
+          //Checkout ID no longer exists
+          this.resultText = 'This page is no longer valid';
+        }
+
+        if (!this.result.success)
           console.info(JSON.stringify(this.result));
       }
-      catch(error) {
+      catch (error) {
         this.loaded = true;
         console.error(JSON.stringify(error));
+        this.resultText = 'Error occurred, could not update your payment details.';
       }
     });
   }
