5969f5640abe126e6be30d46219fe93d9cf18b77
Dane Williams <danewilliams@danes-mbp.gray.za.ibm.com>
2018-12-11 10:41:03 +0200

    Update emailAdmin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   baseAPIURL: "https://puzzleapi.eu-gb.mybluemix.net",
   applicationOutcomeDays: 7,
-  emailAdmin: "admin@puzzleofmylife.co.za",
+  emailAdmin: "admin@puzzleofmylife.com",
   checkoutFormSrc: "https://test.oppwa.com/v1/paymentWidgets.js",
   baseAppUrl: "https://puzzleapi.eu-gb.mybluemix.net",
 };
