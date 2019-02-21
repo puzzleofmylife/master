534fec450f59527fe40c8e7200575cb0e9e7c444
Dane Williams <danewilliams@danes-mbp.gray.za.ibm.com>
2019-02-21 12:07:57 +0200

    Add loading spinner and toast error to patient session

--- src/app/components/patient-session/patient-session.component.ts
+++ src/app/components/patient-session/patient-session.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from 'src/app/services/session.service';
 import { Session } from 'src/app/models/Session';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-patient-session',
@@ -12,18 +13,25 @@ export class PatientSessionComponent implements OnInit {
   session: Session;
   showPackageNotActiveError: boolean = false;
   showNoPsychError: boolean;
+  loading: boolean;
 
-  constructor(private sessionService: SessionService) { }
+  constructor(
+    private sessionService: SessionService,
+    private toastService: ToastService) { }
 
   ngOnInit() {
+    this.loading = true;
     this.sessionService.getPatientSession().subscribe(response => {
       this.session = response;
+      this.loading = false;
     }, error => {
+      this.loading = false;
       if (error.error.PackageNotActive) {
         this.showPackageNotActiveError = true;
-      } else if(error.error.PsychologistNotFound) {
+      } else if (error.error.PsychologistNotFound) {
         this.showNoPsychError = true;
       } else {
+        this.toastService.setError();
         console.error(JSON.stringify(error));
       }
     });
