a9f387d0551ad2ce17e1217d4da9d1fc5fcad838
Dane Williams <dane.williams@ibm.com>
2019-02-22 10:06:54 +0200

    Subscribe to notifications and reload session psych has been disabled

--- src/app/components/patient-session/patient-session.component.ts
+++ src/app/components/patient-session/patient-session.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SessionService } from 'src/app/services/session.service';
 import { Session } from 'src/app/models/Session';
 import { ToastService } from 'src/app/services/toast.service';
+import { PushService } from 'src/app/services/push.service';
 
 @Component({
   selector: 'app-patient-session',
   templateUrl: './patient-session.component.html',
   styleUrls: ['./patient-session.component.css']
 })
-export class PatientSessionComponent implements OnInit {
+export class PatientSessionComponent implements OnInit, OnDestroy {
 
   session: Session;
   showPackageNotActiveError: boolean = false;
   showNoPsychError: boolean;
   loading: boolean;
+  notificationsSubscription: any;
 
   constructor(
     private sessionService: SessionService,
-    private toastService: ToastService) { }
+    private toastService: ToastService,
+    private pushService: PushService) { }
 
   ngOnInit() {
+    this.getSession();
+    this.subscribeToNotifcations();
+  }
+
+  private getSession() {
     this.loading = true;
     this.sessionService.getPatientSession().subscribe(response => {
       this.session = response;
@@ -28,13 +36,30 @@ export class PatientSessionComponent implements OnInit {
       this.loading = false;
       if (error.error.PackageNotActive) {
         this.showPackageNotActiveError = true;
-      } else if (error.error.PsychologistNotFound) {
+      }
+      else if (error.error.PsychologistNotFound) {
         this.showNoPsychError = true;
-      } else {
+      }
+      else {
         this.toastService.setError();
         console.error(JSON.stringify(error));
       }
     });
+  }
+
+  subscribeToNotifcations(): any {
+    this.notificationsSubscription = this.pushService.getNotifications().subscribe(resp => {
+      var notifTypes = [7];//Psychologist not available (i.e they've been disabled)
+      if (notifTypes.includes(resp.notificationTypeId))
+        this.getSession();
+    }, error => {
+      console.error(JSON.stringify(error));
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.notificationsSubscription)
+      this.notificationsSubscription.unsubscribe();
   }
 
 }
